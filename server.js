fa128841d72013bbbf48030c50c9765bf78cf16e
Sirawitch Tiyasuttipun <beam3ba@gmail.com>
2023-01-27 19:45:40 +0700

    Assignment4

---
+++ server.js
@@ -0,0 +1,8 @@
+const express= require('express')
+const dotenv=require('dotenv')
+const hospitals=require('./routes/hospitals')
+dotenv.config({path:'./config/config.env'})
+const app=express();
+const PORT=process.env.PORT||5000
+app.use('/api/v1/hospitals',hospitals);
+app.listen(PORT,console.log('Server running in',process.env.NODE_ENV,'mode on port',PORT))
