65844f450910f99311c61d3017d6e760bf43eae4
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2023-02-24 23:26:54 +0700

    Update hospitals.js

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,11 +1,45 @@
 const Hospital=require('../models/Hospital');
 exports.getHospitals=async(req,res,next)=>{
-    try{
-        const hospital = await Hospital.find();
-        if(!hospital){
-            return res.status(400).json({success:false})
+        let query;
+        const reqQuery={...req.query};
+        const removeFields=['select','sort','page','limit']
+        removeFields.forEach(param=>delete reqQuery[param]);
+        console.log(reqQuery);
+        let queryStr=JSON.stringify(req.query);
+        queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match=>`$${match}`);
+        query=Hospital.find(JSON.parse(queryStr));
+        if(req.query.select){
+            const fields=req.query.select.split(',').join(' ');
+            query=query.select(fields);
         }
-        res.status(200).json({success:true,data:hospital});
+        if(req.query.sort){
+            const sortBy=req.query.sort.split(',').join(' ');
+            query=query.sort(sortBy);
+        }
+        else{
+            query=query.sort('-createAt');
+        }
+        const page=parseInt(req.query.page,10)||1;
+        const limit=parseInt(req.query.limit,10)||25
+        const startIndex=(page-1)*limit;
+        const endIndex=page*limit;
+        const total=await Hospital.countDocuments();
+        query=query.skip(startIndex).limit(limit);
+    try{
+        
+        const hospitals = await query;
+        const pagination={};
+        if(endIndex<total){
+            pagination.next={
+                page:page+1,limit
+            }
+        }
+        if(startIndex>0){
+            pagination.prev={
+                page:page-1,limit
+            }
+        }
+        res.status(200).json({success:true,count:hospitals.length,pagination,data:hospitals});
     }catch(err){
         res.status(400).json({success:false})
     }
